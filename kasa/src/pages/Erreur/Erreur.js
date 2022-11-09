ca738d50b20d2d6d77ed5ccea938908245139f49
Jeremy Carriere <jeremyo.carriere@gmail.com>
2022-11-09 12:05:40 +0100

    Erreur 404 page setup

--- kasa/src/pages/Erreur/Erreur.js
+++ kasa/src/pages/Erreur/Erreur.js
@@ -1,7 +1,14 @@
+import {Link} from "react-router-dom"
+import styles from './Erreur.module.css'
+
 export default function Erreur() {
     return(
-        <div>
-            <h1>Erreur 404</h1>
+        <div className={styles.errorBlock}>
+            <h1 className={styles.errorNumber}>404</h1>
+            <h2 className={styles.errorText}>Oups! La page que vous demandez n'existe pas.</h2>
+            <p>
+                <Link to="/" className={styles.errorLink}>Retourner sur la page d’accueil</Link>
+            </p>
         </div>
     )
 }
